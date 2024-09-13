6c84e706b43463c674bd40f0ddc4da80d7920321
K0W4 <kowaleskigabriel@gmail.com>
2024-09-13 17:50:48 -0300

    Adaptando para host web

--- js/script.js
+++ js/script.js
@@ -56,31 +56,31 @@ const showWeatherData = async (city) =>
     switch (json.weather[0].main)
     {
       case 'Clear':
-        image.src = '../images/clear.png';
+        image.src = '/images/clear.png';
         break;
   
       case 'Rain':
-        image.src = '../images/rain.png';
+        image.src = '/images/rain.png';
         break;
   
       case 'Snow':
-        image.src = '../images/snow.png';
+        image.src = '/images/snow.png';
         break;
   
       case 'Clouds':
-        image.src = '../images/cloud.png';
+        image.src = '/images/cloud.png';
         break;
   
       case 'Mist':
-        image.src = '../images/mist.png';
+        image.src = '/images/mist.png';
         break;
   
       case 'Haze':
-        image.src = '../images/mist.png';
+        image.src = '/images/mist.png';
         break;
   
       default:
-        image.src = '../images/cloud.png';
+        image.src = '/images/cloud.png';
     }
   
     temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
